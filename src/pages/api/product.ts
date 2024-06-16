6b22f4e4b27dd2e4ba073b5bc2bc20011f724cc8
kinoi1 <agilsupriyanto14@gmail.com>
2024-06-16 20:16:56 +0700

    adding api part 1

---
+++ src/pages/api/product.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+    status: boolean;
+    statusCode: number;
+    
+    data: {
+        id: number;
+        name: string;
+        price: number;
+        size: string;
+    }[];
+};
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+){
+    const data = [{
+        id:1,
+        name:'Baju Baru',
+        price: 500000,
+        size: 'xl',
+    },
+    {
+        id:2,
+        name:'Baju Lama',
+        price: 500000,
+        size: 'l',
+    },
+
+]
+    res.status(200).json({status: true, statusCode: 200, data});
+}
